d656428aaa22a667d70c3d20d53222b15b2c98f7
Saurabh_Upwanshi <saurabhupwanshi69@gmail.com>
2026-09-15 17:16:05 +0530

    Added CharacterVariable program to check vowels and consonants

--- src/java_assignment/CheckNumber.java
+++ src/java_assignment/CheckNumber.java
@@ -1,25 +1,31 @@
-   //Declare an integer and 
-   
-   //check whether the number is positive, negative, or zero.
-
-
 package java_assignment;
 
 import java.util.Scanner;
 
 public class CheckNumber {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int positive;
-		
-		int negative;
-		
-		int zero;
-		
-		
-	}
-
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("\nEnter a number (ya program band karne ke liye 9999 type karein):");
+            int number = sc.nextInt(); 
+            
+            if (number == 9999) {
+                System.out.println("Program band ho gaya.");
+                break; 
+            }
+            
+            if (number > 0) {
+                System.out.println("The number is Positive.");
+            } else if (number < 0) {
+                System.out.println("The number is Negative.");
+            } else {
+                System.out.println("The number is Zero.");
+            }
+        }
+        
+        sc.close();
+    }
 }
