b5d8b039085d603fa8520c82ff33ae1bbcde07aa
Saurabh_Upwanshi <saurabhupwanshi69@gmail.com>
2026-09-15 19:11:41 +0530

    Updated largestnumber program with continuous loop and error handling

--- src/java_assignment/largestnumber.java
+++ src/java_assignment/largestnumber.java
@@ -1,45 +1,45 @@
-    //Declare two integers and print the largest number using relational operators and
-
-    // if-else statement.
-
 package java_assignment;
 
 import java.util.Scanner;
 
 public class largestnumber {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc= new Scanner(System.in);
-		
-		int num1;
-		
-		int num2;
-		
-		System.out.println("enter the num1");
-		
-		 num1 = sc.nextInt();
-		
-		System.out.println("enter the num2");
-		
-		 num2 = sc.nextInt();
-		
-		if (num1>num2) {
-		
-		System.out.println("largest number is:"+ num1);
-		
-		} else if (num2> num1) {
-			
-			System.out.println("largest number is:" + num2);
-			
-		} else {
-			
-			System.out.println("both number are equal");
-			
-		}
-	}
-		
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("\n--- Find the Largest Number ---");
+            System.out.print("Enter the first number (Type 'e' to exit): ");
+            String input = sc.next();
+            
+            if (input.equalsIgnoreCase("e")) {
+                System.out.println("Exiting the program...");
+                break;
+            }
+            
+            try {
+                int num1 = Integer.parseInt(input);
+                
+                System.out.print("Enter the second number: ");
+                int num2 = sc.nextInt();
+                
+                System.out.println("\nResult:");
+                if (num1 > num2) {
+                    System.out.println("Largest number is: " + num1);
+                } else if (num2 > num1) {
+                    System.out.println("Largest number is: " + num2);
+                } else {
+                    System.out.println("Both numbers are equal.");
+                }
+                
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid input! Please enter a valid integer or 'e'.");
+            }
+            
+            System.out.println("-------------------------------");
+        }
+        
+        sc.close();
+    }
 }
-		
-		
-
